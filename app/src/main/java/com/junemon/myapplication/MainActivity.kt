8ff874ed0d600e735d0b8fb81f8906231a0facc0
ian damping <iandamping92@gmail.com>
2022-01-19 11:34:28 +0700

    MVP with Koin

--- app/src/main/java/com/junemon/myapplication/MainActivity.kt
+++ app/src/main/java/com/junemon/myapplication/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity(),MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        presenter.logMe("Android")
+        presenter.logMe("PassingParameters")
     }
 
     override fun logMessage(name: String) {
